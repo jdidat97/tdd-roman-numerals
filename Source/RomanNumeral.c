58f7d8855b3f4e0881093ee76a23c194eecd8a62
jdidat97 <52666728+jdidat97@users.noreply.github.com>
2019-07-15 09:40:59 -0400

    finished roman numeral tdd exercise

--- Source/RomanNumeral.c
+++ Source/RomanNumeral.c
@@ -6,11 +6,70 @@
 #include <stdio.h>
 #include "RomanNumeral.h"
 
-static char * Append(char *output, const char *toAppend)
+void Append(char *output, const char *toAppend)
 {
-   return output + sprintf(output, "%s", toAppend);
+   sprintf(output + strlen(output), "%s", toAppend);
 }
 
 void RomanNumeral_Convert(unsigned input, char *output)
 {
+
+   memset(output, 0, 1000);
+
+   while (input > 0) {
+
+      if (input >= 1000) {
+         Append(output, "M");
+         input -= 1000; 
+      }
+      else if (input >= 900) {
+         Append(output, "CM");
+         input -= 900; 
+      }
+      else if (input >= 500) {
+         Append(output, "D");
+         input -= 500;
+      }
+      else if (input >= 400) {
+         Append(output, "CD");
+         input -= 400;
+      }
+      else if (input >= 100) {
+         Append(output, "C");
+         input -= 100;
+      }
+      else if (input >= 90) {
+         Append(output, "XC");
+         input -= 90;
+      }
+      else if (input >= 50) {
+         Append(output, "L");
+         input -= 50; 
+      }
+      else if (input >= 40) {
+         Append(output, "XL");
+         input -= 40;
+      }
+      else if (input >= 10) {
+         Append(output, "X");
+         input -= 10;
+      }
+      else if (input >= 9) {
+         Append(output, "IX");
+         input -= 9;
+      }
+      else if (input >= 5) {
+         Append(output, "V");
+         input -= 5;
+      }
+      else if (input >= 4) {
+         Append(output, "IV");
+         input -= 4;
+      }
+      else if (input >= 1) {
+         Append(output, "I");
+         input--;
+      }
+
+   }
 }
