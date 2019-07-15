57e234b9ba75408fc34b9d42a982186569a9f6f1
jdidat97 <52666728+jdidat97@users.noreply.github.com>
2019-07-15 11:16:37 -0400

    removed some extra white space

--- Testing/Tests/RomanNumeral_Test.cpp
+++ Testing/Tests/RomanNumeral_Test.cpp
@@ -125,7 +125,6 @@ TEST(RomanNumeral, SuperTest)
    ShouldConvert(99, "XCIX");
    ShouldConvert(100, "C");
    ShouldConvert(153, "CLIII");
-
    ShouldConvert(200, "CC");
    ShouldConvert(257, "CCLVII");
    ShouldConvert(300, "CCC");
@@ -135,5 +134,4 @@ TEST(RomanNumeral, SuperTest)
    ShouldConvert(952, "CMLII");
    ShouldConvert(1000, "M");
    ShouldConvert(3999, "MMMCMXCIX");
-   
 }
