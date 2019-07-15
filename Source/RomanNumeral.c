dc8615c87afcc7946fdaedd2281b81e9353460e8
jdidat97 <52666728+jdidat97@users.noreply.github.com>
2019-07-15 11:23:56 -0400

    moved table struct to c file

--- Source/RomanNumeral.c
+++ Source/RomanNumeral.c
@@ -6,10 +6,32 @@
 #include <stdio.h>
 #include "RomanNumeral.h"
 
-static char *Append(char *output, const char *toAppend)
+void Append(char *output, const char *toAppend)
 {
-   return output + sprintf(output, "%s", toAppend);
+   sprintf(output + strlen(output), "%s", toAppend);
 }
+
+typedef struct
+{
+   int num;
+   const char *romanNumeral;
+} TableEntry_t;
+
+const TableEntry_t romanNumeralTable[] = {
+   { 1000, "M" },
+   { 900, "CM" },
+   { 500, "D" },
+   { 400, "CD" },
+   { 100, "C" },
+   { 90, "XC" },
+   { 50, "L" },
+   { 40, "XL" },
+   { 10, "X" },
+   { 9, "IX" },
+   { 5, "V" },
+   { 4, "IV" },
+   { 1, "I" }
+};
 
 void RomanNumeral_Convert(unsigned input, char *output)
 {
